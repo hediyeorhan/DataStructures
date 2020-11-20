261d2fe527b625931753c70f3a491aa72cb55e87
Hediye Orhan <hediyeorhan2015@gmail.com>
2020-11-20 12:07:06 +0300

    Update KuyrukYapisiveTekYonluBagliListe.c

--- KuyrukYapisiveTekYonluBagliListe.c
+++ KuyrukYapisiveTekYonluBagliListe.c
@@ -71,21 +71,87 @@ void Listele()
         iter=iter->next;
     }
 }
+void EnqueuSondan(int data)
+{
+    if(size==0)
+    {
+        root = (struct Kuyruk*)malloc(sizeof(struct Kuyruk));
+        root->data = data;
+        root->next = NULL;
+        size++;
+    }
+    else
+    {
+        struct Kuyruk *iter = root;
+        struct Kuyruk *temp = (struct Kuyruk*)malloc(sizeof(struct Kuyruk));
+        while(iter->next != NULL)
+        {
+            iter = iter->next;
+        }
+        temp->data = data;
+        iter->next = temp;
+        temp->next = NULL;
+        size++;
+
+    }
+}
+
+int DequeuBastan()
+{
+    int i;
+    if(size==0)
+    {
+        return -1;
+    }
+    if(size==1)
+    {
+        int result = root->data;
+        root=NULL;
+        size--;
+        return result;
+    }
+    else
+    {
+
+        struct Kuyruk *temp = root;
+        int result = root->data;
+        root=root->next;
+        free(temp);
+        size--;
+        return result;
+    }
+}
 int main()
 {
-    Enqueu(10);
-    Enqueu(20);
-    Enqueu(30);
+   /*
+       Enqueu(10);
+       Enqueu(20);
+       Enqueu(30);
+       Listele();
+       Enqueu(40);
+       Enqueu(50);
+       Listele();
+
+       printf("\nDequeu : %d\n", Dequeu());
+       printf("Dequeu : %d\n", Dequeu());
+       Listele();
+       printf("\nDequeu : %d\n", Dequeu());
+       printf("Dequeu : %d\n", Dequeu());
+       Listele();*/
+
+    EnqueuSondan(10);
+    EnqueuSondan(20);
+    EnqueuSondan(30);
     Listele();
-    Enqueu(40);
-    Enqueu(50);
+    EnqueuSondan(40);
+    EnqueuSondan(50);
     Listele();
 
-    printf("\nDequeu : %d\n", Dequeu());
-    printf("Dequeu : %d\n", Dequeu());
+    printf("\nDequeu : %d\n", DequeuBastan());
+    printf("Dequeu : %d\n", DequeuBastan());
     Listele();
-    printf("\nDequeu : %d\n", Dequeu());
-    printf("Dequeu : %d\n", Dequeu());
+    printf("\nDequeu : %d\n", DequeuBastan());
+    printf("Dequeu : %d\n", DequeuBastan());
     Listele();
     return 0;
 }
